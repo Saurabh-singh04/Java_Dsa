d701e894df3e222061fe265f2ce4520c71d4adf4
saurabh-singh04 <saurabhsinghsailu02@gmail.com>
2023-10-10 14:29:28 +0530

    condition statement

---
+++ src/Condition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Condition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the age");
+        int age = sc.nextInt();
+        if(age>=18)
+        {
+            System.out.println("you will give vote");
+        }
+        else
+        {
+            System.out.println("not eligible");
+        }
+    }
+}
