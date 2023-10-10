59d493964dd1da56d50c05c08640100504017646
saurabh-singh04 <saurabhsinghsailu02@gmail.com>
2023-10-10 14:16:57 +0530

    add

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    int a =33;
+    int b =88;
+    int c = a+b;
+    System.out.println(c);
+    }
+}
---
+++ src/Practise.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+    public class Practise
+    {
+        public static void main(String args[])
+        {
+            // int age =18;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the age ");
+            int b = sc.nextInt();
+            System.out.println(b);
+
+        }
+    }
+
