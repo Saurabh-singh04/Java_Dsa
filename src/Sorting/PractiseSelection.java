b2e82d3ef16ae4b4d4f29a2d64b25f1ebd0f6b86
saurabh-singh04 <saurabhsinghsailu02@gmail.com>
2024-01-10 23:20:19 +0530

    Add PractiseSelection class to Sorting package

--- src/Sorting/PractiseSelection.java
+++ src/Sorting/PractiseSelection.java
@@ -1,2 +1,34 @@
-package Sorting;public class PractiseSelection {
+package Sorting;
+
+public class PractiseSelection {
+    static int[]  SelectionSort(int a[])
+    {
+        for(int i =0;i<a.length-1;i++)
+        {
+            int minPos = i;
+            for(int j =i+1;j<a.length;j++)
+            {
+                if(a[minPos]>a[j])
+                {
+                    minPos = j;
+                }
+            }
+            int temp = a[minPos];
+            a[minPos ]= a[i];
+            a[i]=temp;
+        }
+        return  a;
+    }
+    public static void main(String args[])
+    {
+        int arr[] = { 3,4,5,2,1};
+        int a[]=   SelectionSort(arr);
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+
+
+    }
+
 }
